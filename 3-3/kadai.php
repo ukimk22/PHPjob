8b8ebf74839f37298502425a2695b302aa46284d
ukimk22 <ukimk22@gmail.com>
2023-03-31 22:25:29 +0900

    ３章練習問題２

---
+++ 3-3/kadai.php
@@ -0,0 +1,7 @@
+<form action="result2.php" method="post">
+    <p>０〜９の番号を使って好きな数字の羅列を入力してください。</p>
+    <br>
+    <input type="text" name="number" />
+
+    <input type="submit" value="占う" />
+</form>
---
+++ 3-3/result2.php
@@ -0,0 +1,35 @@
+<?php
+
+$number = $_POST['number'];
+
+$date = date("Y/-m/-d", time());
+
+
+$unsei = mt_rand();
+
+
+if ($unsei = 0) {
+    echo "凶";
+} elseif ($unsei >= 1 && $unsei <= 3) {
+    echo "小吉";
+} elseif ($unsei >= 4 && $unsei >= 6) {
+    echo "中吉";
+} elseif ($unsei = 7 || $unsei = 8) {
+    echo "吉";
+} elseif ($unsei = 9) {
+    echo "大吉";
+}
+
+
+
+
+
+
+printf("%sの運勢は%sです。", $date, $unsei);
+
+
+
+echo '</br>';
+
+//echo "選ばれた数字は";
+//echo mt_rand(0, 10);
